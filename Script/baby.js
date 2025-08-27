105c4a3fde73a14cd1ac248ef32e5935786fa4a7
pakhi86643 <pakhi86643@gmail.com>
2025-08-27 19:22:25 +0600

    Create baby.js

---
+++ Script/baby.js
@@ -0,0 +1,6 @@
+{
+  "author": "ULLASH × IMRAN",
+  "code": 200,
+  "message": "𝐂𝐘𝐁𝐄𝐑'𝐒 𝐉𝐀𝐍 𝐒𝐈𝐌 𝐀𝐏𝐈 𝐈𝐒 𝐑𝐔𝐍𝐍𝐈𝐍𝐆 𝐎𝐍𝐋𝐈𝐍𝐄",
+  "use": "/simsimi?text=Hi | /teach?ask=Hi&ans=Hello | /list | /edit | /delete | /baby?cmd=top-t"
+}
